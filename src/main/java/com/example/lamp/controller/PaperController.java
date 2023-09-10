31a86f1a7243d212d69991e048ea6b633798be10
suohyoon <dbstndh12@gmail.com>
2023-09-10 10:59:16 +0900

    .

--- src/main/java/com/example/lamp/controller/PaperController.java
+++ src/main/java/com/example/lamp/controller/PaperController.java
@@ -8,18 +8,20 @@ import com.itextpdf.text.pdf.PdfWriter;
 import com.example.lamp.service.BibleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+
+import java.io.*;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -34,11 +36,31 @@ public class PaperController {
     private List<Ccm> ccmList = new LinkedList<>();
     private static final String UPLOAD_DIR = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "static" + File.separator + "ccm";
   // CcmDao의 의존성 주입
-    @Autowired
     public PaperController(BibleService bibleService, CcmDao dao, ResourceLoader resourceLoader) {
         this.dao = dao;
         this.bibleService = bibleService;
         this.resourceLoader = resourceLoader;
+    }
+
+    // PDF 파일로 저장
+    @RequestMapping("/{fileName}")
+    public ResponseEntity<Resource> resourceFileDownload(@PathVariable String fileName) {
+        try {
+            Resource resource = resourceLoader.getResource("file:/C:/Devroot/temp/" + fileName);
+            File file = resource.getFile();
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+                    .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM.toString())
+                    .body(resource);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
     // 주보 생성 페이지로 이동
     @GetMapping("/create")
@@ -47,7 +69,6 @@ public class PaperController {
         model.addAttribute("ccmList", ccmList);
         return "/create";
     }
-    // 교독문 가져오기
 
     // 성경 구절 가져오기
     @PostMapping("/bible")
