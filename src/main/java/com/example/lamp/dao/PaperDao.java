e2867ea109967910ff9fddbd0432edbd74eafc04
SuOhYoon <dbstndh12@gmail.com>
2023-09-10 10:56:39 +0900

    2023-09-10 : debugging

--- src/main/java/com/example/lamp/dao/PaperDao.java
+++ src/main/java/com/example/lamp/dao/PaperDao.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 @Mapper
 public interface PaperDao {
-    int createpaper(@Param("title")String title, @Param("guidename")String guidename, @Param("prayname")String prayname, @Param("respname")String respname, @Param("offername")String offername,  @Param("ccm1")String ccm1,  @Param("ccm2")String ccm2,  @Param("ccm3")String ccm3,  @Param("ccm4")String ccm4, @Param("todaybible")String todaybible, @Param("paragraph")String paragraph, @Param("sentence")String sentence, @Param("notice")String notice, @Param("notice1")String notice1, @Param("notice2")String notice2, @Param("notice3")String notice3, @Param("speachname")String speachname, @Param("nprayname")String nprayname, @Param("nrespname")String nrespname, @Param("noffername")String noffername); // 주보 생성
+    int createpaper(@Param("title")String title, @Param("guidename")String guidename, @Param("prayname")String prayname, @Param("respname")String respname, @Param("offername")String offername,  @Param("ccm1")String ccm1,  @Param("ccm2")String ccm2,  @Param("ccm3")String ccm3,  @Param("ccm4")String ccm4, @Param("resp") String resp, @Param("long_label")String long_label, @Param("chapter")int chapter, @Param("start")int start, @Param("fin")int fin, @Param("notice")String notice, @Param("notice1")String notice1, @Param("notice2")String notice2, @Param("notice3")String notice3, @Param("speachname")String speachname, @Param("nprayname")String nprayname, @Param("nrespname")String nrespname, @Param("noffername")String noffername); // 주보 생성
     List<Paper> list(); // 주보 목록
     Paper paper(int idx); // 주보 보기
+    List<Paper> getbible(int idx); // 주보 목록 보여질 때 성경구절 가져오기
     void save(); // PDF 파일로 저장
 }
--- src/main/java/com/example/lamp/service/PaperServiceImpl.java
+++ src/main/java/com/example/lamp/service/PaperServiceImpl.java
@@ -12,8 +12,8 @@ import java.util.List;
 public class PaperServiceImpl implements PaperService{
     private final PaperDao dao;
     @Override
-    public int createpaper(String title, String guidename, String prayname, String respname, String offername, String ccm1, String ccm2, String ccm3, String ccm4, String todaybible, String paragraph, String sentence, String notice, String notice1, String notice2, String notice3, String speachname, String nprayname, String nrespname, String noffername) {
-        return dao.createpaper(title, guidename, prayname, respname, offername, ccm1, ccm2, ccm3, ccm4, todaybible, paragraph, sentence, notice, notice1, notice2, notice3, speachname, nprayname, nrespname, noffername);
+    public int createpaper(String title, String guidename, String prayname, String respname, String offername, String ccm1, String ccm2, String ccm3, String ccm4, String resp, String long_label, int chapter, int start, int fin, String notice, String notice1, String notice2, String notice3, String speachname, String nprayname, String nrespname, String noffername) {
+        return dao.createpaper(title, guidename, prayname, respname, offername, ccm1, ccm2, ccm3, ccm4, resp, long_label, chapter, start, fin, notice, notice1, notice2, notice3, speachname, nprayname, nrespname, noffername);
     }
 
     @Override
@@ -24,5 +24,10 @@ public class PaperServiceImpl implements PaperService{
     @Override
     public Paper paper(int idx) {
         return dao.paper(idx);
+    }
+
+    @Override
+    public List<Paper> getbible(int idx) {
+        return dao.getbible(idx);
     }
 }
